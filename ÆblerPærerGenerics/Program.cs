d85f63d442715d88e765198b6bc3fad49f387ff5
Magnus Emil Mortensen <magn1596@edu.easj.dk>
2017-02-27 13:02:50 +0100

    override

--- ÆblerPærerGenerics/Program.cs
+++ ÆblerPærerGenerics/Program.cs
@@ -10,30 +10,52 @@ namespace ÆblerPærerGenerics
     {
         static void Main(string[] args)
         {
+
+
+            var æble1 = new Æbler() { Navn = "Cox Orange", Lager = 22, Pris = 10.0M };
+            var æble2 = new Æbler() { Navn = "Cox Orange", Lager = 22, Pris = 10.0M };
+            Console.WriteLine("æble1 equals æble2: " + æble1.Equals(æble2));
+
+            Æbler æble3 = æble2;
+            Console.WriteLine("æble3 equals æble2: " + æble3.Equals(æble2));
+
+            var æble4 = new Æbler() { Navn = "Cox Orange", Lager = 1, Pris = 25.0M };
+            Console.WriteLine("æble4 equals æble1: " + æble4.Equals(æble1));
+
+
+
+
+
+
+
+
+
+
+
             //Varebeholdning af Æbler
 
-            var æbleBeholdning = new List<Æbler>();
-            æbleBeholdning.Add(new Æbler() { Navn = "Ingrid Marie", Lager = 50, Pris = 20.0M });
-            æbleBeholdning.Add(new Æbler() { Navn = "Cox Orange", Lager = 25, Pris = 10.0M });
-            æbleBeholdning.Add(new Æbler() { Navn = "Gråsten", Lager = 10, Pris = 22.0M });
-            æbleBeholdning.Add(new Æbler() { Navn = "Guldborg", Lager = 100, Pris = 9.0M });
-            æbleBeholdning.Add(new Æbler() { Navn = "LøgÆble", Lager = 4, Pris = 5.0M });
+            //var æbleBeholdning = new List<Æbler>();
+            //æbleBeholdning.Add(new Æbler() { Navn = "Ingrid Marie", Lager = 50, Pris = 20.0M });
+            //æbleBeholdning.Add(new Æbler() { Navn = "Cox Orange", Lager = 25, Pris = 10.0M });
+            //æbleBeholdning.Add(new Æbler() { Navn = "Gråsten", Lager = 10, Pris = 22.0M });
+            //æbleBeholdning.Add(new Æbler() { Navn = "Guldborg", Lager = 100, Pris = 9.0M });
+            //æbleBeholdning.Add(new Æbler() { Navn = "LøgÆble", Lager = 4, Pris = 5.0M });
 
 
 
-            var pærerBeholdning = new List<Pærer>();
-            pærerBeholdning.Add(new Pærer() { Navn = "Conference", Lager = 75M, Pris = 5M });
-            pærerBeholdning.Add(new Pærer() { Navn = "Concorde", Lager = 33M, Pris = 12M });
-            pærerBeholdning.Add(new Pærer() { Navn = "Glorød Williams", Lager = 10, Pris = 15M });
-            pærerBeholdning.Add(new Pærer() { Navn = "Noveau Poiteau", Lager = 5M, Pris = 22M });
-            pærerBeholdning.Add(new Pærer() { Navn = "Tongre", Lager = 7M, Pris = 21M });
+            //var pærerBeholdning = new List<Pærer>();
+            //pærerBeholdning.Add(new Pærer() { Navn = "Conference", Lager = 75M, Pris = 5M });
+            //pærerBeholdning.Add(new Pærer() { Navn = "Concorde", Lager = 33M, Pris = 12M });
+            //pærerBeholdning.Add(new Pærer() { Navn = "Glorød Williams", Lager = 10, Pris = 15M });
+            //pærerBeholdning.Add(new Pærer() { Navn = "Noveau Poiteau", Lager = 5M, Pris = 22M });
+            //pærerBeholdning.Add(new Pærer() { Navn = "Tongre", Lager = 7M, Pris = 21M });
 
 
-            decimal værdiTotalÆbler = FrugtHandler.CalculateSumÆbler(æbleBeholdning);
-            Console.WriteLine("Så meget er æblebeholdningen værd : " + værdiTotalÆbler );
+            //decimal værdiTotalÆbler = FrugtHandler.CalculateSumÆbler(æbleBeholdning);
+            //Console.WriteLine("Så meget er æblebeholdningen værd : " + værdiTotalÆbler );
 
-            decimal værdiTotalPærer = FrugtHandler.CalculateSumPærer(pærerBeholdning);
-            Console.WriteLine("Så meget er pærerbeholdningen værd: " + værdiTotalPærer);
+            //decimal værdiTotalPærer = FrugtHandler.CalculateSumPærer(pærerBeholdning);
+            //Console.WriteLine("Så meget er pærerbeholdningen værd: " + værdiTotalPærer);
 
 
             Console.ReadLine();
--- ÆblerPærerGenerics/Æbler.cs
+++ ÆblerPærerGenerics/Æbler.cs
@@ -11,6 +11,28 @@ namespace ÆblerPærerGenerics
         public string Navn { get; set; }
         public decimal Pris { get; set; }
         public int Lager { get; set; }
+
+
+
+
+        public override bool Equals(object obj)
+        {
+            Æbler æble = obj as Æbler;
+            if (this.Navn == æble.Navn && this.Pris == æble.Pris)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        public override int GetHashCode()
+        {
+            return Navn.GetHashCode() ^ Pris.GetHashCode();
+        }
     }
 
 }
